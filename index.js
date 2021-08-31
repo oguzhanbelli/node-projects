f7bb60985e66d3b1408a44b690e2004ee5f798e0
Oğuzhan Belli <oguzhanbellii@gmail.com>
2021-09-01 00:53:31 +0300

    Task 5 comp

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+
+    const url = req.url;
+
+
+    if (url === "/") {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write("<h1>İndex Sayfası</h1>");
+    } else if (url === "/about") {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write("Hakkında Sayfası");
+    } else if (url === "/contact") {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write("İletişim Sayfası");
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' })
+        res.write("<h1>404  Sayfa Bulunamadı</h1>");
+    }
+
+
+    res.end();
+
+});
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Sunucu Port ${port}'de başladı`);
+})
+
+
