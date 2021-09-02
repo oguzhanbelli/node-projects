41cce1f3cb765bcbce8e8f23e7ccc77ab03c087c
Oğuzhan Belli <oguzhanbellii@gmail.com>
2021-09-02 12:04:52 +0300

    update Task6

--- tasks/task6.js
+++ tasks/task6.js
@@ -1,22 +1,26 @@
 const Koa = require('koa');
 const app = new Koa();
 
+
+
 // response
 app.use(ctx => {
-  if(ctx.url === "/"){
-    ctx.body = '<h1> INDEX SAYFASINA HOSGELDINIZ </h1>';
-  }else if(ctx.url === "/about"){
-    ctx.body = '<h1> ABOUT SAYFASINA HOSGELDINIZ </h1>';
-  }else if(ctx.url === "/contact"){
-    ctx.body = '<h1> CONTACT SAYFASINA HOSGELDINIZ </h1>';
-  }else{
-    ctx.body = '<h1> 404 SAYFA BULUNAMADI </h1>';
-  }
+    const url = ctx.url;
+    if (url === "/") {
+        ctx.body = '<h1> INDEX SAYFASINA HOSGELDINIZ </h1>';
+    } else if (url === "/about") {
+        ctx.body = '<h1> ABOUT SAYFASINA HOSGELDINIZ </h1>';
+
+    } else if (url === "/contact") {
+        ctx.body = '<h1> CONTACT SAYFASINA HOSGELDINIZ </h1>';
+    } else {
+        ctx.body = '<h1> 404 SAYFA BULUNAMADI </h1>';
+    }
 
 });
 
 const port = 3000;
 
-app.listen(port,()=>[
+app.listen(port, () => [
     console.log(`Sunucu port ${port}'de başladı`)
 ]);
